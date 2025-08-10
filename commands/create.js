66b419eb7b0b80b2f3c8c1b1103ab4679858c85e
Shehu Muhammad <shehuaamuhammad@gmail.com>
2025-08-10 00:14:36 -0400

    modified create command

--- commands/create.js
+++ commands/create.js
@@ -4,8 +4,9 @@ const create = new Command('create')
   .description('Creates a new project folder with boilerplate files.')
   .argument('<name>', 'name of project')
   .option('--template <type>', 'project template type (node, python, react)')
+  .option('--git', 'initializes a Git repo automatically')
   .action((name, options) => {
-    console.log(`Creating project "${name}" with template: ${options.template}`);
+    console.log(`Creating project "${name}" with template: ${options.template} and ${options.git}`);
   });
 
 export default create;
